08d2be936ca6414415bc1b56a8a5567fc4d2ef02
lixueping <lixueping@kernelsoft.com>
2022-10-16 23:13:02 +0800

    新增断点续传文件

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,18 @@ import (
 	"flag"
 
 	"github.com/lflxp/lflxp-static/pkg"
+	"github.com/lflxp/lflxp-static/pkg/sync"
 )
 
 var (
-	portHttpStatic       string
-	staticPortHttpStatic string
-	pathHttpStatic       string
-	isVideo              bool
-	pagesize             int
-	types                string
-	raw                  bool
+	portHttpStatic                string
+	staticPortHttpStatic          string
+	pathHttpStatic                string
+	isVideo                       bool
+	pagesize                      int
+	types                         string
+	raw, debug                    bool
+	src, dest, clean, cleanString string
 )
 
 func init() {
@@ -23,22 +25,39 @@ func init() {
 	flag.StringVar(&types, "t", ".avi,.wma,.rmvb,.rm,.mp4,.mov,.3gp,.mpeg,.mpg,.mpe,.m4v,.mkv,.flv,.vob,.wmv,.asf,.asx", "Filter video types, multiple separated by commas")
 	flag.BoolVar(&isVideo, "v", false, "Switch to video mode or not")
 	flag.BoolVar(&raw, "r", false, "Switch to Raw HTTP URL")
+	flag.BoolVar(&debug, "debug", false, "是否开启断点续传debug日志")
 	flag.IntVar(&pagesize, "c", 20, "Videos per page")
+	flag.StringVar(&src, "src", "", "复制文件原文件或文件夹")
+	flag.StringVar(&dest, "dest", "", "复制文件目标文件或文件夹")
+	flag.StringVar(&clean, "clean", "", "删除文件或文件夹,如：/tmp")
+	flag.StringVar(&cleanString, "contains", "", "删除文件名包含的内容，如：.temp")
 	flag.Parse()
 }
 
 func main() {
-	api := pkg.Apis{
-		StaticPort: staticPortHttpStatic,
-		Port:       portHttpStatic,
-		Path:       pathHttpStatic,
-		Types:      types,
-		IsVideo:    isVideo,
-		PageSize:   pagesize,
-		Raw:        raw,
-	}
-	err := api.Execute()
-	if err != nil {
-		panic(err)
+	if src != "" && dest != "" {
+		err := sync.LocalDirSync(src, dest, debug)
+		if err != nil {
+			panic(err)
+		}
+	} else if clean != "" && cleanString != "" {
+		err := sync.Clean(clean, cleanString)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		api := pkg.Apis{
+			StaticPort: staticPortHttpStatic,
+			Port:       portHttpStatic,
+			Path:       pathHttpStatic,
+			Types:      types,
+			IsVideo:    isVideo,
+			PageSize:   pagesize,
+			Raw:        raw,
+		}
+		err := api.Execute()
+		if err != nil {
+			panic(err)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -5,19 +5,14 @@ go 1.16
 require (
 	github.com/DeanThompson/ginpprof v0.0.0-20190408063150-3be636683586
 	github.com/chenjiandongx/ginprom v0.0.0-20191227144730-e11ebf56bc05
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
-	github.com/elazarl/go-bindata-assetfs v1.0.1
 	github.com/gin-contrib/multitemplate v0.0.0-20200226145339-3e397ee01bc6
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-openapi/spec v0.20.4 // indirect
 	github.com/jroimartin/gocui v0.4.0
-	github.com/jteeuwen/go-bindata v3.0.7+incompatible // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/nsf/termbox-go v0.0.0-20200204031403-4d2b513ad8be // indirect
 	github.com/prometheus/client_golang v1.5.1
-	github.com/swaggo/files v0.0.0-20190704085106-630677cd5c14
-	github.com/swaggo/gin-swagger v1.2.0
 	github.com/swaggo/swag v1.7.6
 	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
